6c79415b11c14455bf3d6cc580ee97346d61127b
Pavel <ntcnntcn1@gmail.com>
2017-11-20 09:47:09 +0100

    change code

--- index.php
+++ index.php
@@ -1,115 +1,97 @@
 <?php
-//1. Создать класс Cat:
+
 class Cat
+{
+    static $canFly = false;
+    public $name;
+    public $age;
+    public $hairColor = array();
+    public $isAlive;
+    const paws = 4;
+
+
+    function __construct($name, $age, $hairColor)
+
     {
-        static $canFly = false;
-        public $name;
-        public $age;
-        public $hairColor = array();
-        public $isAlive;
-        const lapki = 4;
+        $this->name = $name;
+        $this->age = $age;
+        $this->hairColor = $hairColor;
+        $this->isAlive = true;
+    }
+
+    function __destruct()
+    {
+        $this->isAlive = false;
+        echo '<br>Cat #' . $this->name . ' was killed by dicructor';
+    }
 
 
-        function __construct($name, $age, $hairColor, $isAlive)
-
-        {
-            $this->name = $name;
-            $this->age = $age;
-            $this->hairColor = $hairColor;
-            $this->isAlive = $isAlive;
-        }
-
-        function __destruct()
-        {
-           $this->isAlive = false;
-           echo '<br>Cat #'.$this->name.' was killed by dicructor';
-           }
-
-
-        public function error($err)
-        {
-            Try{
-                if ($err = true)
-                {throw new Exception("<br>That's my error");}
-            }catch (Exception $e){
-                echo$e->getMessage();
-            }
-
-
-        }
-
+    public function error()
+    {
+        Try
+        {throw new Exception("<br>That's my error");}
 
 
     }
 
 
+}
+
 
 $cats = array();
-    for ($i=1; $i<6; $i++)
-        {
-            $cats[$i] = new Cat ($i,$i,array ('red','black','white'),true);
-            echo '<br>Cat #'.$i.' was created';
-        }
-
+for ($i = 1; $i < 6; $i++) {
+    $cats[$i] = new Cat ($i, $i, array('red', 'black', 'white'));
+    echo '<br>Cat #' . $i . ' was created';
+}
 
 
 $summ = 0;
 $mult = 1;
-    for ($i=1; $i<6; $i++)
-        {
-            $summ = $summ + $cats[$i]->age;
-            $mult = $mult * $cats[$i]->age;
-        }
-    echo '<br> Summ of age is '.$summ.' and mult of age is '.$mult;
+for ($i = 1; $i <= count($cats); $i++) {
+    $summ = $summ + $cats[$i]->age;
+    $mult = $mult * $cats[$i]->age;
+}
+echo "<br> Summ of age is \n" . $summ . ' and mult of age is ' . $mult;
 
 
-
-    for ($i=1; $i<6; $i++) {
-        if ($cats[$i]->age>3)
-        {
-            $meny = $meny + 1;
-        }
-        }
-
-    if ($meny >0)
-        echo '<br>  There is cat over 3 years old';
-
-    if ($meny >1)
-        {
-            for ($j=1; $j<6; $j++) {
-                if ($cats[$j]->age>3)
-                    {
-                        echo '<br>  Cat #'.$j.' is over 3 years old';
-                        $old = $old + ($cats[$j]->age * $cats[$j]->age);
-            }
-        }
-
+for ($i = 1; $i <= count($cats); $i++) {
+    if ($cats[$i]->age > 3) {
+        $meny = $meny + 1;
     }
-    echo '<br> Old is '.$old;
+}
 
+if ($meny > 0)
+    echo "<br> There is cat over 3 years old";
 
-
-    $cats[rand(1,5)]->error(false);
-
-
-
-    for ($i=1; $i<6; $i++)
-    {
-        for ($j=1; $j<6; $j++)
-        {
-            if ($i*$j % 29)
-                goto label;
+$old = 0;
+if ($meny > 1) {
+    for ($i = 1; $i <= count($cats); $i++) {
+        if ($cats[$i]->age > 3) {
+            echo "<br>  Cat #" . $i . 'is over 3 years old';
+            $old = $old + sqrt($cats[$i]->age);
         }
     }
 
-    label:
+}
+echo '<br> Old is ' . $old;
 
 
+$cats[rand(1, 5)]->error();
 
-    if ($canFly == false)
-    {
-        echo '<br> Cats cannot fly';}
-        else
-        {
-            echo '<br>Cats can fly after kick';
-        }
+
+for ($i = 1; $i < 100; $i++) {
+    for ($j = 1; $j < 100; $j++) {
+        if ($i * $j % 29)
+            goto label;
+    }
+}
+
+label:
+
+
+if ($canFly == false) {
+    echo '<br> Cats cannot fly';
+} else {
+    echo '<br>Cats can fly after kick';
+}
+
