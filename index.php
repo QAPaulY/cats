454dcc43f06fbd74cd7076194ab8c0bd25139994
Pavel <ntcnntcn1@gmail.com>
2017-11-24 09:02:43 +0100

    code comments update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-class Cat
-{
+class Cat//initiation class cat
+{//variable initiation
     static $canFly = false;
     public $name;
     public $age;
@@ -10,19 +10,19 @@ class Cat
     const paws = 4;
 
 
-    function __construct($name, $age, $hairColor)
+    function __construct($name, $age, $hairColor)//constructor function
 
-    {
+    {//assignment of values to variables
         $this->name = $name;
         $this->age = $age;
         $this->hairColor = $hairColor;
         $this->isAlive = true;
     }
 
-    function __destruct()
+    function __destruct()//delete all created items
     {
         $this->isAlive = false;
-        echo '<br>Cat #' . $this->name . ' was killed by dicructor';
+        echo "\n Cat #" . $this->name . ' was killed by dicructor';
     }
 
 
@@ -36,48 +36,48 @@ class Cat
 }
 
 
-$cats = array();
-for ($i = 1; $i < 11; $i++) {
-   $cats[$i] = new Cat ($i, $i+1, array('red', 'black', 'white'));
-    echo '<br>Cat #' . $i . ' was created';
+$cats = array(); //initiation array cats
+for ($i = 1; $i < 11; $i++) { //creation items of 'cats' class
+    $cats[$i] = new Cat ($i, $i + 1, array('red', 'black', 'white'));
+    echo "\n Cat #" . $i . ' was created';
 }
 
 
 $summ = 0;
 $mult = 1;
-for ($i = 1; $i <= count($cats); $i++) {
+for ($i = 1; $i <= count($cats); $i++) {//counting the amount and the product of the growths of all cats
     $summ = $summ + $cats[$i]->age;
     $mult = $mult * $cats[$i]->age;
 }
-echo "<br> Summ of age is \n" . $summ . ' and mult of age is ' . $mult;
+echo "\n  Summ of age is " . $summ . ' and mult of age is ' . $mult;
 
 
 for ($i = 1; $i <= count($cats); $i++) {
-    if ($cats[$i]->age > 3) {
+    if ($cats[$i]->age > 3) {//checking for cats over 3 years old
         $meny = $meny + 1;
     }
 }
 
 if ($meny > 0)
-    echo "<br> There is cat over 3 years old";
+    echo "\n  There is cat over 3 years old";
 
 $old = 0;
-if ($meny > 1) {
+if ($meny > 1) {//the addition of the square roots of the cat's sprouts older than 3 years
     for ($i = 1; $i <= count($cats); $i++) {
         if ($cats[$i]->age > 3) {
-            echo "<br>  Cat #" . $i . 'is over 3 years old';
+            echo "\n   Cat #" . $i . 'is over 3 years old';
             $old = $old + sqrt($cats[$i]->age);
         }
     }
 
 }
-echo '<br> Old is ' . $old;
+echo "\n  Old is " . $old;
 
 
-$cats[rand(1, 5)]->error();
+$cats[rand(1, 5)]->error();//calling error function for random item of cats
 
 
-for ($i = 1; $i < 100; $i++) {
+for ($i = 1; $i < 100; $i++) {//The nested loop checks the division of the value by 29 without residue
     for ($j = 1; $j < 100; $j++) {
         if ($i * $j % 29)
             goto label;
@@ -87,9 +87,9 @@ for ($i = 1; $i < 100; $i++) {
 label:
 
 
-if ($canFly == false) {
-    echo '<br> Cats cannot fly';
+if ($canFly == false) {//check the ability of cats to fly
+    echo "\n  Cats cannot fly";
 } else {
-    echo '<br>Cats can fly after kick';
+    echo "\n Cats can fly after kick ";
 }
 
